ee94a8255497527d12bccd39ea74e14343baed48
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-03-25 10:36:07 +0800

    Create variables to select by using querySelector
    - class, id and tag were used as selector

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const squares = document.querySelector('.container');
+const grid = document.querySelector('.grid');
+const gridSize = document.querySelector('.grid-size');
+const buttons = document.querySelector('.buttons')
+const sizeBtn = document.querySelector('#grid-size-btn')
+const clearGrid = document.querySelector('#clear-grid-btn')
+
+const divSize = document.createElement('div');
