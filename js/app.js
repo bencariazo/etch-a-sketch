6a5dfb4c3d44e166a38369b70f5aac92ab066e1e
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-03-25 10:52:06 +0800

    Create function askSize
    - gets input
    - test input with conditions
    - returns input if it pass the conditions

--- js/app.js
+++ js/app.js
@@ -8,3 +8,15 @@ const clearGrid = document.querySelector('#clear-grid-btn')
 const divSize = document.createElement('div');
 divSize.textContent = '';
 
+function askSize() {
+    let askSize = Number(prompt("What is the new size of your grid? (Max: 100)"));
+    function getSize(input) {
+        if(isNaN(input) || input === '' || input > 100){
+            return false;
+        } else {
+            return askSize;
+        }
+    }
+    return getSize(askSize)
+}
+
